b5904dbdc805bf7960aab170cf1f3a7bced4b498
Nahuel Lezcano <nlezcano733@gmail.com>
2021-04-22 22:31:48 -0300

    #4 - se agregan hooks

--- src/components/itemListContainer/itemListContainer.js
+++ src/components/itemListContainer/itemListContainer.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { ItemCount } from '../itemCount/itemCount';
 
 import './itemListContainer.css';
 
 export const ItemListContainer = (props) =>{
     return (
-        <div className="container my-4">
+        <div className="container mt-4 mb-20">
             <div className="col-four card">
                 <div className="py-8 h-full bg-gray-400 rounded-lg text-center">inserte imagen</div>
                 <h2 className="text-center mt-4">{props.title}</h2>
+                < ItemCount initial="1" stock="5"/>
             </div>
         </div>
     )
--- src/components/menu-burger/menuBurger.js
+++ src/components/menu-burger/menuBurger.js
@@ -1,31 +1,28 @@
 import React from 'react';
+import {useRef} from 'react';
 
 import './menuBurger.css';
 
 export const MenuBurger = () => {
         let show = 'toggle-show';
         let hide = 'toggle-hide';
+        const sidebar = useRef()
 
-    const showMenu = () =>{
-        let sidebar = document.querySelector('#sidebar__nav');
-
-        sidebar.classList.remove(hide);
-        sidebar.classList.add(show);
-    }
-
-    const hideMenu = () =>{
-        let sidebar = document.querySelector('#sidebar__nav');
-
-        sidebar.classList.remove(show);
-        sidebar.classList.add(hide);
+    const handleCLick = ({type}) =>{
+        if(type == "open"){
+            sidebar.current.className = `sidebar ${show}`
+        }
+        if(type == "close"){
+            sidebar.current.className = `sidebar ${hide}`
+        }
     }
 
     return (
         <div className="block md:hidden text-white">
-            <i className="fas fa-bars text-2xl px-2" id="sidebar" onClick={showMenu}></i>
+            <i className="fas fa-bars text-2xl px-2" id="sidebar" onClick={() => {handleCLick({type: 'open'})}}></i>
 
-            <div className="sidebar" id="sidebar__nav" >
-                <i className="fas fa-times sidebar-exit" id="sidebar__exit" onClick={hideMenu}></i>
+            <div className="sidebar" ref={sidebar} >
+                <i className="fas fa-times sidebar-exit" id="sidebar__exit" onClick={() => {handleCLick({type: 'close'})}}></i>
                 <nav className=" sidebar-nav">
                     <a className="sidebar-links" href="">Home</a>
                     <a className="sidebar-links" href="">Nosotros</a>
