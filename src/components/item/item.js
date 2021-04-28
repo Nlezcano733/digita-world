8b2f9be57da3fe129e4c1cfbf4c5ecf19ffd76a1
Nahuel Lezcano <nlezcano733@gmail.com>
2021-04-28 12:34:48 -0300

    solucion a problema de rutas relativas

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -4,9 +4,11 @@ import { ItemCount } from '../itemCount/itemCount';
 import './item.css';
 
 export const Item = (props) => {
+  console.log(props.picture)
   return (
     <div className="card col-four">
         <div className="img-container">
+            {/* <img src={"./banner.jpg"} className="img-cover" /> */}
             <img src={props.picture} className="img-cover" />
         </div>
         <h1 className="title">{props.title}</h1>
